49d8251aab3901190b5cfe9d30c9e4dc4b07078f
GabriellaMullerBorges <gabimullerborges@gmail.com>
2024-02-21 18:31:28 -0300

    test: testes do botão e da lista

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,13 +1,54 @@
 import { render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
+import { waitFor } from '@testing-library/react';
 import App from './App'
 
-// obs: elementos que começam c Query não falharão caso o elemento não seja encontradol
-test ('sum', () => {
-    const { getByText } = render(<App/>)
+// obs: elementos que começam c Query não falharão caso o elemento não seja encontrado
+//find espera o elemento aparecer em tela 
 
+test ('title', () => {
+    const { getByText } = render(<App />);
     const testElement = getByText('Projeto de Teste');
     expect(testElement).toBeInTheDocument;
     expect(testElement).toHaveClass('test');
-
 })
+
+describe('App Component', () => {
+    it('it should be able to add new itens to the list' , () =>{
+        const { getByText } = render(<App />);
+      expect(getByText('Java')).toBeInTheDocument;  
+      expect(getByText('JavaScript')).toBeInTheDocument; 
+    } )
+});
+
+
+describe('App Component', () => {
+    it('should change the button color to red on click', async () => {
+      const { getByText } = render(<App />);
+      const addButton = getByText('Adicionar');
+  
+      userEvent.click(addButton);
+  
+      await waitFor(() => {
+        expect(addButton).toHaveStyle('background-color: red');
+      })
+    });
+  });
+
+  describe('App Component', () => {
+    it('it should render a list of itens regarding programming languages' , async  () =>{
+        const { getByText, debug } = render(<App />);
+        const addButton = getByText('Adicionar');
+    
+        debug();
+        expect(addButton).toBeInTheDocument;
+        userEvent.click(addButton);    
+
+        await waitFor(() => {
+            expect(getByText('Python')).toBeInTheDocument
+          })
+          debug()
+       
+    } )
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [list, setList] = useState(['Java', 'JavaScript', 'C#']);
+  const [isClicked, setIsClicked] = useState(false);
+
+  function addList() {
+    setList((state) => [...state, 'Python']);
+    setIsClicked(true); // Altera o estado para indicar que o botão foi clicado
+  }
 
   return (
     <>
       <h1 className='test'>Projeto de Teste</h1>
+      <button
+        onClick={addList}
+        style={{ backgroundColor: isClicked ? 'red' : 'initial' }} // Muda a cor do botão para vermelho se foi clicado
+      >
+        Adicionar
+      </button>
+      <ul className='lista'>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
